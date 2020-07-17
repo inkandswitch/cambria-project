c6bd157b0956f10da8f4cf24b907cb37365e2111
Geoffrey Litt <gklitt@gmail.com>
2020-07-17 14:07:07 -0400

    tweak exports

--- src/cli.ts
+++ src/cli.ts
@@ -1,62 +1,58 @@
 // A CLI document conversion tool for cloudina
 
-import { program } from "commander";
-import { readFileSync } from "fs";
-import YAML from "js-yaml";
-import { convertDoc } from "./cloudina";
-import { LensSource, LensOp } from "./lens-ops";
-import { reverseLens } from "./reverse";
+import { program } from 'commander'
+import { readFileSync } from 'fs'
+import YAML from 'js-yaml'
+import { convertDoc } from './index'
+import { LensSource, LensOp } from './lens-ops'
+import { reverseLens } from './reverse'
 
 interface YAMLLens {
-  lens: LensSource;
+  lens: LensSource
 }
 
 // copied from migrationRunner.ts; should probably migrate into cloudina
 const foldInOp = (lensOpJson): LensOp => {
-  const opName = Object.keys(lensOpJson)[0];
+  const opName = Object.keys(lensOpJson)[0]
 
   // the json format is
   // {"<opName>": {opArgs}}
   // and the internal format is
   // {op: <opName>, ...opArgs}
-  const data = lensOpJson[opName];
-  if (["in", "map"].includes(opName)) {
-    data.lens = data.lens.map((lensOp) => foldInOp(lensOp));
+  const data = lensOpJson[opName]
+  if (['in', 'map'].includes(opName)) {
+    data.lens = data.lens.map((lensOp) => foldInOp(lensOp))
   }
 
-  const op = { op: opName, ...data };
-  return op;
-};
+  const op = { op: opName, ...data }
+  return op
+}
 
 program
-  .requiredOption("-l, --lens <filename>", "lens source as yaml")
-  .option("-i, --input <filename>", "input document filename")
-  .option("-b, --base <filename>", "base document filename")
-  .option("-r, --reverse", "run the lens in reverse");
+  .requiredOption('-l, --lens <filename>', 'lens source as yaml')
+  .option('-i, --input <filename>', 'input document filename')
+  .option('-b, --base <filename>', 'base document filename')
+  .option('-r, --reverse', 'run the lens in reverse')
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 // read doc from stdin if no input specified
-const input = readFileSync(program.input || 0, "utf-8");
-const baseDoc = program.base
-  ? JSON.parse(readFileSync(program.base, "utf-8"))
-  : {};
-const doc = JSON.parse(input);
-const rawLens = YAML.safeLoad(readFileSync(program.lens, "utf-8")) as YAMLLens;
+const input = readFileSync(program.input || 0, 'utf-8')
+const baseDoc = program.base ? JSON.parse(readFileSync(program.base, 'utf-8')) : {}
+const doc = JSON.parse(input)
+const rawLens = YAML.safeLoad(readFileSync(program.lens, 'utf-8')) as YAMLLens
 
-if (!rawLens || typeof rawLens !== "object")
-  throw new Error("Error loading lens");
-if (!("lens" in rawLens))
-  throw new Error(`Expected top-level key 'lens' in YAML lens file`);
+if (!rawLens || typeof rawLens !== 'object') throw new Error('Error loading lens')
+if (!('lens' in rawLens)) throw new Error(`Expected top-level key 'lens' in YAML lens file`)
 
 // we could have a root op to make this consistent...
 let lens = (rawLens.lens as LensSource)
   .filter((o) => o !== null)
-  .map((lensOpJson) => foldInOp(lensOpJson));
+  .map((lensOpJson) => foldInOp(lensOpJson))
 if (program.reverse) {
-  lens = reverseLens(lens);
+  lens = reverseLens(lens)
 }
 
-const newDoc = convertDoc(doc, lens, baseDoc);
+const newDoc = convertDoc(doc, lens, baseDoc)
 
-console.log(JSON.stringify(newDoc, null, 4));
+console.log(JSON.stringify(newDoc, null, 4))
--- src/patch.ts
+++ src/patch.ts
@@ -1,11 +1,14 @@
-import { Operation } from 'fast-json-patch'
+import { Operation, compare, applyPatch } from 'fast-json-patch'
 import { LensSource, LensOp } from './lens-ops'
+import { reverseLens } from './reverse'
+import { defaultObjectForLens } from './defaults'
 
 // todo: we're throwing away the type param right now so it doesn't actually do anything.
 // can we actually find a way to keep it around and typecheck patches against a type?
 export type PatchOp = Operation
 type MaybePatchOp = PatchOp | null
 export type Patch = Operation[]
+type CompiledLens = (patch: Patch, targetDoc: any) => Patch
 
 function assertNever(x: never): never {
   throw new Error(`Unexpected object: ${x}`)
@@ -13,6 +16,39 @@ function assertNever(x: never): never {
 
 function noNulls<T>(items: (T | null)[]) {
   return items.filter((x): x is T => x !== null)
+}
+
+// Provide curried functions that incorporate the lenses internally;
+// this is useful for exposing a pre-baked converter function to developers
+// without them needing to access the lens themselves
+// TODO: the public interface could just be runLens and reverseLens
+// ... maybe also composeLens?
+export function compile(lensSource: LensSource): { right: CompiledLens; left: CompiledLens } {
+  return {
+    right: (patch: Patch, targetDoc: any) => applyLensToPatch(lensSource, patch, targetDoc),
+    left: (patch: Patch, targetDoc: any) =>
+      applyLensToPatch(reverseLens(lensSource), patch, targetDoc),
+  }
+}
+
+// utility function: converts a document (rather than a patch) through a lens
+export function convertDoc(doc: any, lensSource: LensSource, baseDoc?: any) {
+  // build up a patch that creates the document
+  const patchForOriginalDoc = compare({}, doc)
+
+  // convert the patch through the lens
+  const convertedPatch = applyLensToPatch(lensSource, patchForOriginalDoc, {})
+
+  // construct the "base" upon which we will apply the patches from doc.
+  // it's a combination of default fields for the lens,
+  // and an existing "base doc" which we merge the converted doc into.
+  // base doc is helpful in cases where, for example, doc doesn't have all the fields in the
+  // target schema, so baseDoc needs to define those missing fields.
+  const base = Object.assign(defaultObjectForLens(lensSource), baseDoc || {})
+
+  // return a doc based on the converted patch.
+  // (start with either a specified baseDoc, or just empty doc)
+  return applyPatch(base, convertedPatch).newDocument
 }
 
 export function applyLensToPatch(lensSource: LensSource, patch: Patch, destinationDoc: any): Patch {
